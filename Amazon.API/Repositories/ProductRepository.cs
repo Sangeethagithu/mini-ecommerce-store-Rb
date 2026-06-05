2b3b7f3073686db1887a1b1facdc7f3a276051c0
Sangeetha Priya Sankar <MSIN003@merlinchennai.loc>
2026-06-05 18:01:30 +0530

    levenshtein distance

--- Amazon.API/Repositories/ProductRepository.cs
+++ Amazon.API/Repositories/ProductRepository.cs
@@ -284,6 +284,19 @@ namespace Amazon.API.Repositories
             }
 
             connection.Close();
+            if (products.Count == 0)
+            {
+                var allProducts =
+                    GetAllProducts();
+
+                products =
+                    allProducts
+                    .Where(p =>
+                        LevenshteinDistance(
+                            p.Name.ToLower(),
+                            name.ToLower()) <= 3)
+                    .ToList();
+            }
 
             return products;
         }
@@ -371,6 +384,43 @@ namespace Amazon.API.Repositories
             connection.Close();
 
             return products;
+        }
+
+
+        private int LevenshteinDistance(
+    string s,
+    string t)
+        {
+            int[,] d =
+                new int[
+                    s.Length + 1,
+                    t.Length + 1];
+
+            for (int i = 0; i <= s.Length; i++)
+                d[i, 0] = i;
+
+            for (int j = 0; j <= t.Length; j++)
+                d[0, j] = j;
+
+            for (int i = 1; i <= s.Length; i++)
+            {
+                for (int j = 1; j <= t.Length; j++)
+                {
+                    int cost =
+                        s[i - 1] == t[j - 1]
+                        ? 0
+                        : 1;
+
+                    d[i, j] =
+                        Math.Min(
+                            Math.Min(
+                                d[i - 1, j] + 1,
+                                d[i, j - 1] + 1),
+                            d[i - 1, j - 1] + cost);
+                }
+            }
+
+            return d[s.Length, t.Length];
         }
     }
 }
